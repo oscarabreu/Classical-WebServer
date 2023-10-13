ba94460735787b97b2e093966d26a636b162c7f9
Oscar Abreu <oja227@nyu.edu>
2023-10-12 23:12:09 -0400

    Restful API Server

--- main.go
+++ main.go
@@ -1,42 +1,107 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+type Form struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
 
+var forms = make(map[int]Form)
+var idCounter int
+var formsMutex sync.Mutex
 
+func getAllFormsHandler(w http.ResponseWriter, r *http.Request) {
+	formsMutex.Lock()
+	defer formsMutex.Unlock()
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
+	var formList []Form
+	for _, form := range forms {
+		formList = append(formList, form)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(formList)
+}
+
+func getFormHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if form, exists := forms[id]; exists {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(form)
+	} else {
+		http.Error(w, "Form not found", http.StatusNotFound)
+	}
+}
+
+func createFormHandler(w http.ResponseWriter, r *http.Request) {
+	var newForm Form
+	if err := json.NewDecoder(r.Body).Decode(&newForm); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
+
+	formsMutex.Lock()
+	idCounter++
+	newForm.ID = idCounter
+	forms[idCounter] = newForm
+	formsMutex.Unlock()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newForm)
+}
+
+func updateFormHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	var updatedForm Form
+	if err := json.NewDecoder(r.Body).Decode(&updatedForm); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Hello!")
+
+	formsMutex.Lock()
+	defer formsMutex.Unlock()
+
+	if _, exists := forms[id]; exists {
+		updatedForm.ID = id
+		forms[id] = updatedForm
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(updatedForm)
+	} else {
+		http.Error(w, "Form not found", http.StatusNotFound)
+	}
+}
+
+func deleteFormHandler(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+
+	formsMutex.Lock()
+	defer formsMutex.Unlock()
+
+	if _, exists := forms[id]; exists {
+		delete(forms, id)
+		w.WriteHeader(http.StatusNoContent)
+	} else {
+		http.Error(w, "Form not found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/api/forms", getAllFormsHandler)
+	http.HandleFunc("/api/form", getFormHandler)
+	http.HandleFunc("/api/form/create", createFormHandler)
+	http.HandleFunc("/api/form/update", updateFormHandler)
+	http.HandleFunc("/api/form/delete", deleteFormHandler)
 
 	fmt.Print("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
